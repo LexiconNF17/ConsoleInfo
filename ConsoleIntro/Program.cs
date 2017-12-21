77f32569d9332985723b63cb5eddf16a7b39b426
Adrian Lozano <adrian@xenotype.com>
2017-12-21 08:33:03 +0100

    Add project files.

---
+++ ConsoleIntro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIntro
+{
+    class Program
+    {
+        static void Main()
+        {
+            int val1 = AskForInt("Ange värde 1");            
+            int val2 = AskForInt("Ange värde 2");
+
+            // Jämför
+            var max = Util.Max(val1, val2);
+
+            Console.WriteLine($"Du angav {val1} och {val2}");
+            Console.WriteLine($"Det högsta värdet var {max}");
+        }
+
+        private static int AskForInt(string question)
+        {
+            int val;
+            bool parsed;
+            do
+            {
+                Console.WriteLine(question);
+                var input = Console.ReadLine();
+                parsed = int.TryParse(input, out val);
+                if (!parsed)
+                {
+                    Console.WriteLine("Fel format");
+                }
+            } while (!parsed);
+            return val;
+        }
+    }
+}
